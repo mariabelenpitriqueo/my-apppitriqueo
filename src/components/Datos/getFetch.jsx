f60e84494353d9afcd38d8be66e7a684575b93c1
mariabelenpitriqueo <mb.pitriqueo@gmail.com>
2021-12-14 15:54:07 +0100

    Adjunte imagenes a los items
    a modo de ejemplo, agregue imagenes reales a los items simulando  productos en venta

--- src/components/Datos/getFetch.jsx
+++ src/components/Datos/getFetch.jsx
@@ -1,11 +1,11 @@
 const products = [
     {id: 1, photo:"src/public/image/picture1.jpg", name:'cuadro1', autor:'Vincent vanGoht', price: '$89' },
-    {id: 2, photo:'', name:'cuadro2', autor: 'Claude Monet', price: '$190'},
-    {id: 3, photo:'', name:'cuadro3', autor: 'Paul Cezanne', price: '$190'},
-    {id: 4, photo:'', name:'cuadro4', autor: 'Claude Monet', price: '$190'},
-    {id: 5, photo:'', name:'cuadro5', autor: 'Claude Monet', price: '$190'},
-    {id: 6, photo:'', name:'cuadro6', autor: 'Claude Monet', price: '$190'},
-    {id: 7, photo:'', name:'cuadro7', autor: 'Claude Monet', price: '$190'},
+    {id: 2, photo:'src/public/image/picture1.jpg', name:'cuadro2', autor: 'Claude Monet', price: '$190'},
+    {id: 3, photo:'src/public/image/picture1.jpg', name:'cuadro3', autor: 'Paul Cezanne', price: '$190'},
+    {id: 4, photo:'src/public/image/picture1.jpg', name:'cuadro4', autor: 'Claude Monet', price: '$190'},
+    {id: 5, photo:'src/public/image/picture1.jpg', name:'cuadro5', autor: 'Claude Monet', price: '$190'},
+    {id: 6, photo:'src/public/image/picture1.jpg', name:'cuadro6', autor: 'Claude Monet', price: '$190'},
+    {id: 7, photo:'src/public/image/picture1.jpg', name:'cuadro7', autor: 'Claude Monet', price: '$190'},
 ];
 export const getFetch = new Promise ((resolve) => {
     setTimeout(()=>{
