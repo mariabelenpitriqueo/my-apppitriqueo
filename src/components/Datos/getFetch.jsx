7a19f5e413ec2cdf881aafddb5ca580cec145a49
mariabelenpitriqueo <mb.pitriqueo@gmail.com>
2021-12-16 23:21:20 +0100

    Correccion Ortografica
    correccion de nombre al ingles "graphic art"

--- src/components/Datos/getFetch.jsx
+++ src/components/Datos/getFetch.jsx
@@ -4,8 +4,8 @@ const products = [
     {id: 3, photo:'src/public/image/picture1.jpg', name:'cuadro3', autor: 'Paul Cezanne', price: '$190',category: "Pictures"},
     {id: 4, photo:'src/public/image/picture1.jpg', name:'cuadro4', autor: 'Claude Monet', price: '$190',category: "Vintage"},
     {id: 5, photo:'src/public/image/picture1.jpg', name:'cuadro5', autor: 'Claude Monet', price: '$190',category: "Vintage"},
-    {id: 6, photo:'src/public/image/picture1.jpg', name:'cuadro6', autor: 'Claude Monet', price: '$190',category: "GrafictArt"},
-    {id: 7, photo:'src/public/image/picture1.jpg', name:'cuadro7', autor: 'Claude Monet', price: '$190',category: "GrafictArt"},
+    {id: 6, photo:'src/public/image/picture1.jpg', name:'cuadro6', autor: 'Claude Monet', price: '$190',category: "GraphicArt"},
+    {id: 7, photo:'src/public/image/picture1.jpg', name:'cuadro7', autor: 'Claude Monet', price: '$190',category: "GraphicArt"},
 ];
 export const getFetch = new Promise ((resolve) => {
     setTimeout(()=>{
